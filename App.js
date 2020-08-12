faf87a67c87194edf2c9bdb11e060b9304117925
StephBerg86 <info@stephaniebergman.nl>
2020-08-12 10:16:35 +0200

    select multiple images

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import AddTipScreen from "./app/screens/AddTipScreen";
-import SelectImage from "./app/components/SelectImage";
+import SelectImage from "./app/components/AddTipForm/SelectImage";
 
 export default function App() {
   return <SelectImage />;
---
+++ app/components/AddTipForm/SelectImage.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import Screen from "../Screen";
+import ImageInputList from "./ImageInputList";
+
+export default function SelectImage() {
+  const [imageUris, setImageUris] = useState([]);
+
+  const handleAdd = (uri) => {
+    setImageUris([...imageUris, uri]);
+  };
+
+  const handleRemove = (uri) => {
+    setImageUris(imageUris.filter((imageUri) => imageUri !== uri));
+  };
+  return (
+    <Screen>
+      <ImageInputList
+        imageUris={imageUris}
+        onAddImage={handleAdd}
+        onRemoveImage={handleRemove}
+      />
+    </Screen>
+  );
+}
